4f25207b53ce2e1a12ee9d28ccb7f9dd0b16fba8
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2024-12-23 14:16:03 +0100

    first commit

---
+++ src/main/java/com/capstone/africa/semicolon/comms_bridge/CommsBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.africa.semicolon.comms_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommsBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommsBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/africa/semicolon/comms_bridge/CommsBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.africa.semicolon.comms_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommsBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
